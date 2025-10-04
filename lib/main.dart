1695f867aa33676a2dbab984a385a95655540ab6
Humais <humaissamsung@gmail.com>
2025-10-04 20:55:56 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:my_app/firebase_options.dart';
+import 'package:my_app/views/login_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home'), backgroundColor: Colors.blue),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print('You are verified');
+              } else {
+                print("You need to verify your email");
+              }
+              return const Text('Done');
+            default:
+              return Center(
+                child: Text(
+                  "Loading...",
+                  style: TextStyle(fontSize: 39, color: Colors.blue),
+                ),
+              );
+          }
+        },
+      ),
+    );
+  }
+}
