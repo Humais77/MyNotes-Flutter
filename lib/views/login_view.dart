74104bbb669213a2c298bd7d3399915acca50d9a
Humais <humaissamsung@gmail.com>
2025-10-08 13:17:41 +0500

    add the home route

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -49,18 +50,19 @@ class _LoginViewState extends State<LoginView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                final userCredential = await FirebaseAuth.instance
-                    .signInWithEmailAndPassword(
-                      email: email,
-                      password: password,
-                    );
+                await FirebaseAuth.instance.signInWithEmailAndPassword(
+                  email: email,
+                  password: password,
+                );
 
-                print(userCredential);
+                Navigator.of(
+                  context,
+                ).pushNamedAndRemoveUntil('/appview/', (route) => false);
               } on FirebaseAuthException catch (e) {
                 if (e.code == "invalid-credential") {
-                  print("Invalid Credentials");
+                  devtools.log("Invalid Credentials");
                 } else if (e.code == "wrong-password") {
-                  print("Wrong Password");
+                  devtools.log("Wrong Password");
                 }
               }
             },
